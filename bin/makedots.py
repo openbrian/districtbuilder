f7483a9d1772c812caca0e3ff2e61efc8b42b240
Brian DeRocher <brian@derocher.org>
2014-05-31 10:07:13 -0400

    * Add python interpreter shabang. * Remove unused code regarding states list. * Set working state to Virginia.

--- bin/makedots.py
+++ bin/makedots.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import os
 import sys
 import ogr
@@ -195,8 +196,6 @@ if __name__=='__main__':
 	print "Downloading states: %s:"%states
 	starttime=time()
 	
-	#for state in ['01','02','04','05','06','08','09','10','11','12','13','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','44','45','46','47','48','49','50','51','53','54','55','56']:
-  
   	if confirm("Step 1: Download files, Continue?", True):
 		# download all data from ftp://ftp2.census.gov/geo/tiger/TIGER2010BLKPOPHU/
 		downloadlist=""
